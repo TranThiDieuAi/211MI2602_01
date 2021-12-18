476d89926fdefd4c0178154f1ebd82f565b8766e
Nguyen Thu Van <vannt19406c@st.uel.edu.vn>
2021-12-18 18:38:09 +0700

    ThuVan-update payment

--- javascript/base.js
+++ javascript/base.js
@@ -21,3 +21,34 @@ app.controller('UserController', function($scope, $http) {
             $scope.dataError = response.statusText;
         });
 })
+
+// payment function
+function decreaseNumber() {
+    var quantity = document.getElementById("quantity");
+    var price = document.getElementById("price")
+    var total_amount = document.getElementById("amount");
+
+    if (quantity.value <= 0) {
+        quantity.value = 0;
+    } else {
+        quantity.value = parseInt(quantity.value) - 1;
+    }
+    // total amount
+    total_amount.innerHTML = quantity.value * parseInt(price.innerText) + ".000đ"
+}
+
+function increaseNumber() {
+    var quantity = document.getElementById("quantity");
+    var price = document.getElementById("price")
+    var total_amount = document.getElementById("amount");
+    quantity.value = parseInt(quantity.value) + 1;
+    total_amount.innerHTML = quantity.value * parseInt(price.innerText) + ".000đ"
+}
+
+function openForm() {
+    document.getElementById('myForm').style.display = 'flex';
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
