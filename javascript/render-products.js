f8b503bdc22b59ad320bc5b54e71473e7b250237
manhquang173 <93414312+manhquang173@users.noreply.github.com>
2021-12-23 14:59:27 +0700

    heyy

--- javascript/render-products.js
+++ javascript/render-products.js
@@ -1,51 +1,43 @@
-function myFunction(x) {
-    if (x.matches) {
-        fetch("../data/products.json")
-            .then(function(response) {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error("HTTP error, status: " + response.status)
-                }
-            })
-            .then(function(arr) {
-                var div = "";
-                var i;
+let urlparams = new URLSearchParams(location.search);
+var id = urlparams.get('productID');
+let app = angular.module("AngularApp", []);
+app.controller("DetailController", function($scope, $http) {
+    $http({
+        method: "GET",
+        url: "../data/products.json"
+    }).then(
+        function success(response) {
 
-                for (i = 0; i < 10; i++) {
-                    div += "<div class='col-12 col-sm-4 col-md-3 col-lg-2dot4 col-xl-2'><a href='" + arr[i].url + "'><div class='card bestseller--item'><img class='card-img-top bestseller--item-img' src='" + arr[i].icon_url + "'><div class='card-body bestseller--item-content'><p class='card-text bestseller--item-name'>" + arr[i].name + "</p><p class='card-text bestseller--item-price'>" + arr[i].price + "</p></div></div></a></div>"
-                }
-                // div += "</div>";
-                document.getElementById("bestseller_row").innerHTML = div;
+            $scope.products = response.data;
+
+            $scope.selectProduct = response.data.find(function(value) {
+                return value.name == id;
             })
-            .catch(function(error) {
-                alert("Error: " + error.message);
-            });
+        },
+        function error(response) {
+            $scope.error = response.statusText;
+        }
+    )
+})
+
+function decreaseNumber() {
+    var quantity = document.getElementById("quantity");
+    var price = document.getElementById("price")
+    var total_amount = document.getElementById("amount");
+
+    if (quantity.value <= 0) {
+        quantity.value = 0;
     } else {
-        fetch("../data/products.json")
-            .then(function(response) {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error("HTTP error, status: " + response.status)
-                }
-            })
-            .then(function(arr) {
-                var div = "";
-                var i;
-
-                for (i = 0; i < 25; i++) {
-                    div += "<div class='col-8 col-sm-4 col-md-3 col-lg-2dot4'><a href='" + arr[i].url + "'><div class='card bestseller--item'><img class='card-img-top bestseller--item-img' src='" + arr[i].icon_url + "'><div class='card-body bestseller--item-content'><p class='card-text bestseller--item-name'>" + arr[i].name + "</p><p class='card-text bestseller--item-price'>" + arr[i].price + "</p></div></div></a></div>"
-                }
-                // div += "</div>";
-                document.getElementById("bestseller_row").innerHTML = div;
-            })
-            .catch(function(error) {
-                alert("Error: " + error.message);
-            });
+        quantity.value = parseInt(quantity.value) - 1;
     }
+    // total amount
+    total_amount.innerHTML = quantity.value * parseInt(price.innerText) + ".000đ"
 }
 
-var x = window.matchMedia("(max-width: 1200px) and (min-width: 992px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+function increaseNumber() {
+    var quantity = document.getElementById("quantity");
+    var price = document.getElementById("price")
+    var total_amount = document.getElementById("amount");
+    quantity.value = parseInt(quantity.value) + 1;
+    total_amount.innerHTML = quantity.value * parseInt(price.innerText) + ".000đ"
+}
