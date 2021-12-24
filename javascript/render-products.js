40a9c0cb1b0067c926ce6f4e1818d4e68a6f26f4
manhquang173 <93414312+manhquang173@users.noreply.github.com>
2021-12-24 15:28:05 +0700

    hey

--- javascript/render-products.js
+++ javascript/render-products.js
@@ -7,12 +7,35 @@ app.controller("DetailController", function($scope, $http) {
         url: "../data/products.json"
     }).then(
         function success(response) {
-
+            $scope.carts = [];
+            var quantity = document.getElementById("quantity");
             $scope.products = response.data;
 
             $scope.selectProduct = response.data.find(function(value) {
                 return value.name == id;
             })
+            $scope.add_cart = function(product) {
+                if (product) {
+                    $scope.carts.push({ name: product.name, price: product.price * quantity.value });
+                }
+            }
+
+            $scope.total = 0;
+
+
+
+            $scope.setTotals = function(cart) {
+                if (cart) {
+                    $scope.total += (cart.price * 1);
+                }
+            }
+
+            $scope.remove_cart = function(cart) {
+                if (cart) {
+                    $scope.carts.splice($scope.carts.indexOf(cart), 1);
+                    $scope.total -= cart.price;
+                }
+            }
         },
         function error(response) {
             $scope.error = response.statusText;
@@ -52,4 +75,12 @@ function increaseNumber() {
     var total_amount = document.getElementById("amount");
     quantity.value = parseInt(quantity.value) + 1;
     total_amount.innerHTML = quantity.value * parseInt(price.innerText) + ".000đ"
+}
+
+function openForm() {
+    document.getElementById('myForm').style.display = 'flex';
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
 }
